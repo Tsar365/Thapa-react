d8b7748b3eca4b496135b23dc1d39b5dae35fb50
Ehasanul Haque <ehasanmd365@gmail.com>
2026-07-07 11:57:20 +0600

    9 updated

--- Thapa React/src/App.jsx
+++ Thapa React/src/App.jsx
@@ -1,7 +1,5 @@
-
 function App() {
-  
-  return (
+	return (
 		<>
 			<div>
 				<img
@@ -16,46 +14,185 @@ function App() {
 			<p>Queen of Taers is a powerful warrior from the kingdom of Taers.</p>
 			<NetflixSeries />
 			<NetflixSeries />
-			<NetflixSeries />
-			<NetflixSeries />
-      <Sony />
+			<ConditionalRendering />
+			<Sony />
 		</>
 	);
 }
 
-export default App
+export default App;
 
-const NetflixSeries=()=>{
-  return(
-    <>
-    <div>
-      <img src="/netflix.jpeg" alt="netflix" width="600" height="300"/>
-    </div>
-    <h2>Name: Stranger Things</h2>
-    <h3>Genre: Science Fiction</h3>
-    <p>Stranger Things is a popular Netflix series that combines supernatural elements with 1980s nostalgia.</p>
-    </>
-  )
-}
+const NetflixSeries = () => {
+	return (
+		<>
+			<div>
+				<img
+					src="/netflix.jpeg"
+					alt="netflix"
+					width="600"
+					height="300"
+				/>
+			</div>
+			<h2>Name: Stranger Things</h2>
+			<h3>Genre: Science Fiction</h3>
+			<p>
+				Stranger Things is a popular Netflix series that combines supernatural
+				elements with 1980s nostalgia.
+			</p>
+		</>
+	);
+};
 
-const Sony=()=>{
-  const name="Stranger Things"
-  const genre="Science Fiction"
-  const description="Stranger Things is a popular Netflix series that combines supernatural elements with 1980s nostalgia."
-  const getValue=()=>{
-    const value="Nice Value"
-    return value
+
+//* Way---> 1
+// const ConditionalRendering=()=>{
+//  let age=18;
+
+
+//   return(
+//     <>
+//        <div>
+// 					<img
+// 						src="/condition.jpg"
+// 						alt="condition"
+// 						width="600"
+// 						height="300"
+// 					/>
+// 				</div>
+//     <div>
+//       <button >{age>=18? "Watch Now":"Availabe"}</button>
+//     </div>
+//     </>
+//   )
+//  }
+
+//* Way---> 2
+// const ConditionalRendering = () => {
+// 	let age = 19;
+
+//   let canWatch="Not available";
+//   if (age>=18){
+//     canWatch="Watch now";
+//   }
+
+// 	return (
+// 		<>
+// 			<div>
+// 				<img
+// 					src="/condition.jpg"
+// 					alt="condition"
+// 					width="600"
+// 					height="300"
+// 				/>
+// 			</div>
+// 			<div>
+// 				<button>{canWatch}</button>
+// 			</div>
+// 		</>
+// 	);
+// };
+
+
+//* Way---> 2
+
+const ConditionalRendering = () => {
+	let age = 19;
+
+	const canWatch =()=>{
+	if (age >= 18) {
+		return 'Watch now';
+  } else{
+    return "Not availabe"
   }
-  return(
-    <>
-    <div>
-      <img src="/sony.png" alt="sony" width="600" height="300"/>
-    </div>
-    <h2>Name:{ name }</h2>
-    <h3>Genre: { genre }</h3>
-    <p>{ description }</p>
-    <h3>Rating: {5 + 3.2}</h3>
-    <p>Value from function: {getValue()}</p>
-    </>
-  )
-}
+  }
+	
+
+	return (
+		<>
+			<div>
+				<img
+					src="/condition.jpg"
+					alt="condition"
+					width="600"
+					height="300"
+				/>
+			</div>
+			<div>
+				<button>{canWatch()}</button>
+			</div>
+		</>
+	);
+};
+
+
+
+//* Way---> 4
+// ! **************************Not recommended***************************
+// const ConditionalRendering=()=>{
+//  let age=18;
+
+//  if (age<18){
+//   return(
+//     <>
+//        <div>
+// 					<img
+// 						src="/condition.jpg"
+// 						alt="condition"
+// 						width="600"
+// 						height="300"
+// 					/>
+// 				</div>
+//     <div>
+//       <button>Not available</button>
+//     </div>
+//     </>
+//   )
+//  }
+//  else{
+//     return (
+// 			<>
+// 				<div>
+// 					<img
+// 						src="/condition.jpg"
+// 						alt="condition"
+// 						width="600"
+// 						height="300"
+// 					/>
+// 				</div>
+// 				<div>
+// 					<button>Available</button>
+// 				</div>
+// 			</>
+// 		);
+//  }
+// }
+
+
+
+const Sony = () => {
+	const name = 'Stranger Things';
+	const genre = 'Science Fiction';
+	const description =
+		'Stranger Things is a popular Netflix series that combines supernatural elements with 1980s nostalgia.';
+	const getValue = () => {
+		const value = 'Nice Value';
+		return value;
+	};
+	return (
+		<>
+			<div>
+				<img
+					src="/sony.png"
+					alt="sony"
+					width="600"
+					height="300"
+				/>
+			</div>
+			<h2>Name:{name}</h2>
+			<h3>Genre: {genre}</h3>
+			<p>{description}</p>
+			<h3>Rating: {5 + 3.2}</h3>
+			<p>Value from function: {getValue()}</p>
+		</>
+	);
+};
