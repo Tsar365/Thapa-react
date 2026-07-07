17e3e64c59e0acb30697a99e75a89fcea6b4fb88
Ehasanul Haque <ehasanmd365@gmail.com>
2026-07-07 11:13:19 +0600

    6 updated

--- Thapa React/src/App.jsx
+++ Thapa React/src/App.jsx
@@ -1,17 +1,38 @@
 
 function App() {
   
-
   return (
-   <>
-   <div>
-    <img src="/phoenix.jpeg" alt="phoenix" width="600" height="300"/>
-   </div>
-   <h2>Name: Queen of Taers</h2>
-   <h3>Role: Warrior</h3>
-   <p>Queen of Taers is a powerful warrior from the kingdom of Taers.</p>
-   </>
-  )
+		<>
+			<div>
+				<img
+					src="/phoenix.jpeg"
+					alt="phoenix"
+					width="600"
+					height="300"
+				/>
+			</div>
+			<h2>Name: Queen of Taers</h2>
+			<h3>Role: Warrior</h3>
+			<p>Queen of Taers is a powerful warrior from the kingdom of Taers.</p>
+			<NetflixSeries />
+			<NetflixSeries />
+			<NetflixSeries />
+			<NetflixSeries />
+		</>
+	);
 }
 
 export default App
+
+const NetflixSeries=()=>{
+  return(
+    <>
+    <div>
+      <img src="/netflix.jpeg" alt="netflix" width="600" height="300"/>
+    </div>
+    <h2>Name: Stranger Things</h2>
+    <h3>Genre: Science Fiction</h3>
+    <p>Stranger Things is a popular Netflix series that combines supernatural elements with 1980s nostalgia.</p>
+    </>
+  )
+}
