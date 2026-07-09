6fe5c1b42dd1c0ec9e693378c352e795806ab5c9
Ehasanul Haque <ehasanmd365@gmail.com>
2026-07-10 00:48:53 +0600

    16 updated

--- Thapa React/src/main.jsx
+++ Thapa React/src/main.jsx
@@ -1,12 +1,15 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Profile from './Profile.jsx'
 // import App from './App.jsx'
-import Movie from './Movie.jsx'
+// import Movie from './Movie.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     // <App />
-<Movie/>
+// {/* <Movie/> */}
+<Profile/>
   // {/* </StrictMode>, */}
 )
